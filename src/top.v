e0639d3c38c916a180ad90b5a0c416957ea4bc45
jayraj4021 <jayraj4021@gmail.com>
2023-08-20 18:54:50 +0530

    Update top.v
    added the ports to top module definition as per matt's video

--- src/top.v
+++ src/top.v
@@ -8,7 +8,16 @@
 `include "./alu.v"
 `include "./output_register.v"
 
-module tt_um_jayraj4021_SAP1_cpu(clk, rst);
+module tt_um_jayraj4021_SAP1_cpu #( parameter MAX_COUNT = 10_000_000 ) (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
     input clk;
     input rst;
